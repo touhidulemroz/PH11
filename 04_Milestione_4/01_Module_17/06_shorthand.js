7b0cf54d556471014429c2089622556a1889186d
touhidulemroz <touhidul.emroz@gmail.com>
2025-02-05 10:22:02 +0600

    module complete

---
+++ 04_Milestione_4/01_Module_17/06_shorthand.js
@@ -0,0 +1,15 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+// console.log(price);
+
+var fridge = 350000;
+// fridge = fridge - 50000;
+fridge -= 50000;
+// console.log(fridge);
+
+// multiply 
+var orangePrice = 20;
+// orangePrice = orangePrice*3;
+orangePrice *=3;
+// console.log(orangePrice);
---
+++ 04_Milestione_4/01_Module_17/07_mixedOp.js
@@ -0,0 +1,11 @@
+var first = 'mobarok'
+var second = 'tobarok'
+
+var fullName = first + second ;
+console.log(fullName);
+console.log('40'+2);
+
+console.log(40 - '2');
+console.log('40'-2);
+
+
