bd3ba0a48f2a159d9ab04dce8645d3d0f86e4f9b
touhidulemroz <touhidul.emroz@gmail.com>
2025-02-26 22:51:32 +0600

    update to the hw task

---
+++ 04_Milestione_4/06_Module_20/14_quiz.js
@@ -0,0 +1,10 @@
+// 01 
+const myObject = {
+    a: 1,
+    b: 2,
+    c: 3
+   };
+   
+   for (let prop in myObject) {
+    console.log(myObject[prop]);
+   }
---
+++ 04_Milestione_4/06_Module_20/15_hw.js
@@ -0,0 +1,6 @@
+// object task
+// https://github.com/ProgrammingHero1/js-object-tasks
+
+// string task
+// https://github.com/ProgrammingHero1/js-string-tasks
+
